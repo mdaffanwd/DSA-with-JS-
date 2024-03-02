3c8f330d9edc726295c9f8b05476a7e5abe27be2
Mohammad Affan <mohammedaffan2305@gmail.com>
2024-03-02 08:59:45 +0530

    SearchingElement

---
+++ 5,SearchElement.js
@@ -0,0 +1,12 @@
+/*
+Search Element in Array
+1. Understand Case and solution with WhiteBoard.
+2. Make array.
+3. Use loop to Search element.
+4. Search element with text input and button.
+
+        *. Shortcut for Search element(Default Function).
+        *. Shortcut for Delete element(Default Function).
+*/
+
+let arr = [20, 10, 30, 40, 10, 50, 60]
