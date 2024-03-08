ebf59ad70d2167e318ccd222cf5805043ab75a45
Mohammad Affan <mohammedaffan2305@gmail.com>
2024-03-08 21:58:46 +0530

    merging 2 arrays using while loop

---
+++ 7.Merging_While.js
@@ -0,0 +1,8 @@
+/*
+Merge Two Arrays.
+1. Understand Case and solution with WhiteBoard.
+2. Make three arrays.
+3. Use while Loop to Merge element.
+
+        *. Compare this logic with last one
+*/
