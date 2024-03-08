22118314282c42bc23494fb86dd795560d118e08
Mohammad Affan <mohammedaffan2305@gmail.com>
2024-03-08 21:54:57 +0530

    practiced merging of 2 arrays using for loop

--- zPractice.js
+++ zPractice.js
@@ -1,4 +1,4 @@
-// --------- 3.InsertElement
+//* --------- 3.InsertElement
 /*
 let arr = [1,2,3,4,5,6,7,8,9,10]
 let position = 2;
@@ -13,7 +13,7 @@ for(let i = 0; i< arr.length; i++){
 console.log(arr);
 */
 
-// 4.Deletion-of-Element.
+//* 4.Deletion-of-Element.
 /*
 let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 let position = 2;
@@ -27,7 +27,7 @@ for (let i = 0; i <= arr.length; i++) {
 console.log(arr);
 */
 
-// ------- 5.Search Element in Array.
+//* ------- 5.Search Element in Array.
 /*
 let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 70];
 let item = 70;
@@ -44,4 +44,18 @@ for (let i = 0; i < arr.length; i++) {
     }
 }
 console.log(index, index1);
-*/
+*/              
+
+//* ------- 6. Merging 2 Arrays in a new Array.
+
+let arr = [3, 7, 12, 34, 56, 90];
+let arr2 = [20, 30, 40, 50];
+let arr3 = [] 
+
+for(let i = 0; i<arr.length; i++){
+    arr3[i] = arr[i]
+}
+for(let i =0; i<arr2.length; i++){
+    arr3[arr.length + i] = arr2[i] 
+}
+console.log(arr3);
