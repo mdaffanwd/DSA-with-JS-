b3d5185e088d41782f7016e718e8b723202c1b8d
Mohammad Affan <mohammedaffan2305@gmail.com>
2024-03-09 09:02:38 +0530

    code of merging and sortin of 2 arrays using while loop

--- 7.Merging_While.js
+++ 7.Merging_While.js
@@ -7,4 +7,30 @@ Merge Two Arrays (While Loop).
         *. Compare this logic with last one
 */
 
-let arr = []
+let arr1 = [3, 7, 12, 34, 56, 90];
+let arr2 = [20, 30, 40, 50];
+let arr3 = []
+let d1 = 0;
+let d2 = 0;
+let d3 = 0;
+while (d1 < arr1.length && d2 < arr2.length) {
+        if (arr1[d1] < arr2[d2]) {
+                arr3[d3] = arr1[d1];
+                d1++;
+                // console.log(arr3[d3]);
+
+        } else {
+                arr3[d3] = arr2[d2];
+                d2++;
+                // console.log(arr3[d3]);
+        }
+        d3++;
+}
+// console.log(d1);
+while (d1 < arr1.length) {
+        arr3[d3] = arr1[d1];
+        d1++;
+        d3++
+}
+console.log(arr3);
+
